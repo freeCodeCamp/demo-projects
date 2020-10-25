b9761eb1ba6050b4ca774007752c344b5fb57a52
Kristofer Koishigawa <scissorsneedfoodtoo@gmail.com>
2020-10-25 12:40:53 +0900

    Update scripts/twitch-proxy/updateApiKey.js
    Co-authored-by: Oliver Eyton-Williams <ojeytonwilliams@gmail.com>

--- scripts/twitch-proxy/updateApiKey.js
+++ scripts/twitch-proxy/updateApiKey.js
@@ -6,7 +6,6 @@ const fs = require('fs');
 const { TWITCH_CLIENT_ID, TWITCH_CLIENT_SECRET } = process.env;
 
 const updateApiKey = async () => {
-  const envFileText = fs.readFileSync(envFilePath, 'utf-8');
   const url = `https://id.twitch.tv/oauth2/token?client_id=${TWITCH_CLIENT_ID}&client_secret=${TWITCH_CLIENT_SECRET}&grant_type=client_credentials`;
   const keyObj = await fetch(url, {
     method: 'POST'
