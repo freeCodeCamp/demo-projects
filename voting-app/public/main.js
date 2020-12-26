b1c229c9e54fe55ccdb1392efce28ca3f1f2c86e
moT01 <20648924+moT01@users.noreply.github.com>
2020-12-26 13:00:50 -0600

    feat: add voting app project

---
+++ voting-app/public/main.js
@@ -0,0 +1,19 @@
+/*$(".poll-title").click(function () {
+   $(this).siblings().last().slideToggle("slow");
+});*/
+
+const dontToggle = ['INPUT', 'OPTION', 'SELECT']
+
+$(".poll-wrap").click(function(e) {
+	if(!dontToggle.includes(e.target.tagName)) {
+  	$(this).children().last().slideToggle("slow");
+	}
+});
+
+$('select').change(function () {
+    if ($(this).val() === 'Add option') {
+      $(this).next().prop('disabled', false);
+  } else {
+    $(this).next().prop('disabled', true);
+    }
+});
