1880acc8ed408a25e5226e5348f71f86e287d8b8
Mrugesh Mohapatra <me@raisedadead.com>
2021-03-28 02:17:10 +0530

    feat: enable PM2 for anonymous-message-board

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,34 @@
-const applist = ['american-british-translator'];
+require('dotenv').config();
+
+const applist = [
+  {
+    name: 'american-british-translator'
+  },
+  {
+    name: 'anonymous-message-board',
+    env: {
+      DB_URI: process.env.anonymous_message_board_DB_URI
+    }
+  }
+];
 
 module.exports = {
   apps: applist.map((app, index) => {
+    const { name, env } = app;
     return {
-      name: app,
+      name: name,
       script: `server.js`,
-      cwd: `./apps/${app}`,
+      cwd: `./apps/${name}`,
       watch: true,
       watch_delay: 5000,
       ignore_watch: ['node_modules'],
-      env: {
-        PORT: 50000 + 10 * (index + 1)
-      }
+      env: Object.assign(
+        {},
+        {
+          PORT: 50000 + 10 * (index + 1)
+        },
+        env ? env : {}
+      )
     };
   })
 };
