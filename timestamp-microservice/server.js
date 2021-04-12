631e3dc8c8d0895610cabbf7455180651365697a
Kris Koishigawa <scissorsneedfoodtoo@gmail.com>
2021-04-12 17:26:17 +0900

    fix: support new and legacy endpoints, update packages

--- timestamp-microservice/server.js
+++ timestamp-microservice/server.js
@@ -1,68 +1,52 @@
-'use strict';
-
 require('dotenv').config();
-var express = require('express');
-var app = express();
-
-var cors = require('cors');
+const cors = require('cors');
+const express = require('express');
+const app = express();
 
 app.use(cors({ optionsSuccessStatus: 200 }));
 
-app.use(express.static('public')); 
+app.use(express.static('public'));
 
-app.route('/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-  });
-    
-app.route('/api/timestamp/:date?')
-  .get(function (req, res){
-    
-    var date = null;
-    // parse the date string
-    if (req.params.date !== undefined) {
+app.route('/').get((req, res) => {
+  res.sendFile(`${process.cwd()}/views/index.html`);
+});
 
-      // check if it is a unix timestamp...
-      var unixTimestamp = parseInt(req.params.date*1);
-      if (isNaN(unixTimestamp)) {
-        
-        // it's not a unix timestamp string
-        date = new Date(req.params.date);
-      } else {
-        
-        // it is a timestamp
-        date = new Date(unixTimestamp);
-      }
-      
+app.route(['/api/timestamp/:date?', '/api/:date?']).get((req, res) => {
+  let date = null;
+  // parse the date string
+  if (req.params.date !== undefined) {
+    // check if it is a unix timestamp
+    const unixTimestamp = parseInt(req.params.date * 1);
+    if (isNaN(unixTimestamp)) {
+      // it's not a unix timestamp string
+      date = new Date(req.params.date);
     } else {
-      
-      // the date string parameter is empty. 
-      // create a new date based on current time 
-      date = new Date(Date.now());
+      // it is a timestamp
+      date = new Date(unixTimestamp);
     }
-    
-    // Initialize the response object, if Date is invalid
-    // this one will be returned
+  } else {
+    // the date string parameter is empty.
+    // create a new date based on current time
+    date = new Date(Date.now());
+  }
 
-    var response = date == "Invalid Date" ? 
-      { error: "Invalid Date" } :
-      { "unix": date.getTime(),
-        "utc": date.toUTCString()
-      };
-    
-    res.json(response);
-  });
-    
+  // Initialize the response object, if Date is invalid
+  // this one will be returned
+  const response =
+    date == 'Invalid Date'
+      ? { error: 'Invalid Date' }
+      : { unix: date.getTime(), utc: date.toUTCString() };
+
+  res.json(response);
+});
+
 // 404 Not Found Middleware
-app.use(function(req, res, next) {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
+app.use((req, res, next) => {
+  res.status(404).type('text').send('Not Found');
 });
 
 const portNum = process.env.PORT || 3000;
 
-//Start our server and tests!
-app.listen(portNum, function () {
-  console.log("Listening on port " + portNum);
+app.listen(portNum, () => {
+  console.log(`Listening on port ${portNum}`);
 });
