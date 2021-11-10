2c89d1e510fe62744eff197d579a5fa99c284562
Kris Koishigawa <scissorsneedfoodtoo@gmail.com>
2021-11-10 16:37:45 +0900

    fix: undefined port

--- scripts/twitch-proxy/updateApiKey.js
+++ scripts/twitch-proxy/updateApiKey.js
@@ -3,7 +3,7 @@ const envFilePath = path.join(`${__dirname}/../../twitch-proxy/`, '.env');
 require('dotenv').config({ path: envFilePath });
 const fetch = require('node-fetch');
 const fs = require('fs');
-const { TWITCH_CLIENT_ID, TWITCH_CLIENT_SECRET } = process.env;
+const { TWITCH_CLIENT_ID, TWITCH_CLIENT_SECRET, PORT } = process.env;
 
 const updateApiKey = async () => {
   const url = `https://id.twitch.tv/oauth2/token?client_id=${TWITCH_CLIENT_ID}&client_secret=${TWITCH_CLIENT_SECRET}&grant_type=client_credentials`;
