ef68c7eca2c813a9bb1810b6722b7c0d3c868ded
Kris Koishigawa <scissorsneedfoodtoo@gmail.com>
2022-01-18 17:44:09 +0900

    fix: prettify api.js for image search abstraction layer

--- apps/image-search-abstraction-layer/api.js
+++ apps/image-search-abstraction-layer/api.js
@@ -2,9 +2,8 @@ var googleSearch = require('google-images');
 var moment = require('moment');
 var mongo = require('mongodb').MongoClient;
 
-module.exports = function(app) {
-
-  app.get('/', function(req,res) {
+module.exports = function (app) {
+  app.get('/', function (req, res) {
     console.log("Redirected to 'index.html'");
     return res.sendFile('public/index.html', { root: __dirname });
   });
@@ -17,22 +16,25 @@ module.exports = function(app) {
     var pages = req.query.page;
     var sizes = req.query.size;
     var client = new googleSearch(process.env.CSEID, process.env.APIKEY);
-    
+
     mongo.connect(process.env.MONGO_URI, function (err, db) {
       if (err) throw err;
       console.log('Connected to mongoDB');
-      
+
       var collection = db.collection('searches');
       var time = new Date();
       time /= 1000;
 
-      collection.insert({'searchQuery': searchQuery, 'timeSearched': moment.unix(time).format('MMMM Do YYYY, h:mm:ss a')});    
+      collection.insert({
+        searchQuery: searchQuery,
+        timeSearched: moment.unix(time).format('MMMM Do YYYY, h:mm:ss a'),
+      });
       db.close();
     });
 
-    client.search(searchQuery, {size: sizes, page: pages}).then(images => {
-      return res.json({images});
-     });
+    client.search(searchQuery, { size: sizes, page: pages }).then((images) => {
+      return res.json({ images });
+    });
   }
 
   function getRecent(req, res) {
@@ -40,17 +42,17 @@ module.exports = function(app) {
       var db = client.db();
       if (err) throw err;
       console.log('Connection Established');
-      
+
       var collection = db.collection('searches');
 
-      collection.find( {}, { _id:0 } ).toArray(function(err, result) {
+      collection.find({}, { _id: 0 }).toArray(function (err, result) {
         db.close();
         if (err) return res.send(err);
-        
+
         if (result.length) {
           return res.json(result);
         } else {
-          return res.send('No Documents Found.');        
+          return res.send('No Documents Found.');
         }
       });
     });
