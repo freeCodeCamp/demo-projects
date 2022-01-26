c31470f17ddf28b0fcf1fd730dd6eafb9b00b739
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2022-01-26 11:08:45 +0100

    fix: sync .env parsing to avoid a race condition
    We can't await the parsing, because pm2 will immediately try and use
    the exported config.  This creates a race condition.

--- parse-package.js
+++ parse-package.js
@@ -1,20 +1,10 @@
-const fs = require("fs/promises");
+const fs = require("fs");
 const dotenv = require("dotenv");
 const path = require("path");
 
-async function parsePackage(pkg) {
+function parsePackage(pkg) {
   const filePath = path.resolve(pkg, ".env");
-  try {
-    dotenv.parse(await fs.readFile(filePath));
-  } catch (e) {
-    // TODO: decide if it's ok to only warn if .env files are missing.
-    if (e.code === "ENOENT") {
-      console.warn(`No .env file found in ${pkg}`);
-      return {};
-    } else {
-      throw e;
-    }
-  }
+  return dotenv.parse(fs.readFileSync(filePath));
 }
 
 module.exports = parsePackage;
