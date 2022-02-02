676b262ec047473cb8039db41bf0e6db29d11964
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2022-02-02 17:52:13 +0100

    chore: cleanup

--- apps/p2p-video-chat-application/rollup.config.js
+++ apps/p2p-video-chat-application/rollup.config.js
@@ -1,7 +1,5 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import babel from "@rollup/plugin-babel";
-import replace from "@rollup/plugin-replace";
 import nodePolyfills from 'rollup-plugin-polyfill-node';
 
 export default {
@@ -14,13 +12,8 @@ export default {
   plugins: [
     resolve({ moduleDirectories: ["node_modules", "../../node_modules"] }),
     nodePolyfills(),
-    replace({
-      preventAssignment: true,
-      'process.env.NODE_ENV': JSON.stringify( 'production' )
-    }),
     commonjs({
       include: /node_modules/,
     }),
-    babel({ babelHelpers: "bundled" }),
   ],
 };
