a94112addaf332e133444e1203763e4b3572712c
Kristofer Koishigawa <scissorsneedfoodtoo@gmail.com>
2022-02-09 00:51:33 +0900

    fix: image search abstraction layer recent searches (#98)
    * fix: image search abstraction layer recent searches
    
    * fix: prettify api.js for image search abstraction layer
    
    * fix: use mongo 3 connect callback
    
    Co-authored-by: Oliver Eyton-Williams <ojeytonwilliams@gmail.com>

--- apps/image-search-abstraction-layer/api.js
+++ apps/image-search-abstraction-layer/api.js
@@ -2,9 +2,8 @@ var googleSearch = require('google-images');
 var moment = require('moment');
 var mongo = require('mongodb').MongoClient;
 
-module.exports = function(app) {
-
-  app.get('/', function(req,res) {
+module.exports = function (app) {
+  app.get('/', function (req, res) {
     console.log("Redirected to 'index.html'");
     return res.sendFile('public/index.html', { root: __dirname });
   });
@@ -17,22 +16,26 @@ module.exports = function(app) {
     var pages = req.query.page;
     var sizes = req.query.size;
     var client = new googleSearch(process.env.CSEID, process.env.APIKEY);
-    
-    mongo.connect(process.env.MONGO_URI, function (err, db) {
+
+    mongo.connect(process.env.MONGO_URI, function (err, client) {
+      var db = client.db();
       if (err) throw err;
       console.log('Connected to mongoDB');
-      
+
       var collection = db.collection('searches');
       var time = new Date();
       time /= 1000;
 
-      collection.insert({'searchQuery': searchQuery, 'timeSearched': moment.unix(time).format('MMMM Do YYYY, h:mm:ss a')});    
+      collection.insert({
+        searchQuery: searchQuery,
+        timeSearched: moment.unix(time).format('MMMM Do YYYY, h:mm:ss a'),
+      });
       db.close();
     });
 
-    client.search(searchQuery, {size: sizes, page: pages}).then(images => {
-      return res.json({images});
-     });
+    client.search(searchQuery, { size: sizes, page: pages }).then((images) => {
+      return res.json({ images });
+    });
   }
 
   function getRecent(req, res) {
@@ -40,20 +43,19 @@ module.exports = function(app) {
       var db = client.db();
       if (err) throw err;
       console.log('Connection Established');
-      
+
       var collection = db.collection('searches');
 
-      collection.find( {}, { _id:0 } ).toArray(function(err, result) {
-        if(err) {
-          return res.send(err);
-        } else if (result.length) {
+      collection.find({}, { _id: 0 }).toArray(function (err, result) {
+        db.close();
+        if (err) return res.send(err);
+
+        if (result.length) {
           return res.json(result);
         } else {
-          return res.send('No Documents Found.');        
+          return res.send('No Documents Found.');
         }
       });
-
-      db.close();
     });
   }
 };
