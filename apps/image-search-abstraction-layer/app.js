3169fda075e25d686f7b4adf0d558f6c73b608f9
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2022-02-08 17:24:22 +0100

    fix: use single connection for app process (#160)
    * fix: use single connection for app process
    
    Rather than connecting and closing for each request
    
    * fix: add pretest to create .env in CI

--- apps/image-search-abstraction-layer/app.js
+++ apps/image-search-abstraction-layer/app.js
@@ -1,14 +1,21 @@
 var express = require('express');
 require('dotenv').config();
+var mongo = require('mongodb').MongoClient;
 
 var app = express();
 
 app.use(express.static('public'));
-  
-require('./api.js')(app);
+
+const api = require('./api.js');
 
 var portNum = process.env.PORT || 3000;
 
-app.listen(portNum, () => {
-  console.log(`Listening on port ${portNum}`);
+mongo.connect(process.env.MONGO_URI, function (err, client) {
+  if (err) throw err; 
+  var db = client.db();
+  console.log('Connected to MongoDB');
+  api(app, db);
+  app.listen(portNum, () => {
+    console.log(`Listening on port ${portNum}`);
+  });
 });
