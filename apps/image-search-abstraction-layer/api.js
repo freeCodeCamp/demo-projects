9f6d88409a1521387fc69fc9c974f1d813b4fc48
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2022-02-08 17:11:45 +0100

    fix: use single connection for app process
    Rather than connecting and closing for each request

--- apps/image-search-abstraction-layer/api.js
+++ apps/image-search-abstraction-layer/api.js
@@ -1,8 +1,7 @@
 var googleSearch = require('google-images');
 var moment = require('moment');
-var mongo = require('mongodb').MongoClient;
 
-module.exports = function (app) {
+module.exports = function (app, db) {
   app.get('/', function (req, res) {
     console.log("Redirected to 'index.html'");
     return res.sendFile('public/index.html', { root: __dirname });
@@ -17,45 +16,31 @@ module.exports = function (app) {
     var sizes = req.query.size;
     var client = new googleSearch(process.env.CSEID, process.env.APIKEY);
 
-    mongo.connect(process.env.MONGO_URI, function (err, client) {
-      var db = client.db();
-      if (err) throw err;
-      console.log('Connected to mongoDB');
+    var collection = db.collection('searches');
+    var time = new Date();
+    time /= 1000;
 
-      var collection = db.collection('searches');
-      var time = new Date();
-      time /= 1000;
-
-      collection.insert({
-        searchQuery: searchQuery,
-        timeSearched: moment.unix(time).format('MMMM Do YYYY, h:mm:ss a'),
-      });
-      db.close();
+    collection.insert({
+      searchQuery: searchQuery,
+      timeSearched: moment.unix(time).format('MMMM Do YYYY, h:mm:ss a')
     });
 
-    client.search(searchQuery, { size: sizes, page: pages }).then((images) => {
+    client.search(searchQuery, { size: sizes, page: pages }).then(images => {
       return res.json({ images });
     });
   }
 
-  function getRecent(req, res) {
-    mongo.connect(process.env.MONGO_URI, function (err, client) {
-      var db = client.db();
-      if (err) throw err;
-      console.log('Connection Established');
+  function getRecent(_, res) {
+    var collection = db.collection('searches');
 
-      var collection = db.collection('searches');
+    collection.find({}, { _id: 0 }).toArray(function (err, result) {
+      if (err) return res.send(err);
 
-      collection.find({}, { _id: 0 }).toArray(function (err, result) {
-        db.close();
-        if (err) return res.send(err);
-
-        if (result.length) {
-          return res.json(result);
-        } else {
-          return res.send('No Documents Found.');
-        }
-      });
+      if (result.length) {
+        return res.json(result);
+      } else {
+        return res.send('No Documents Found.');
+      }
     });
   }
 };
