22aa64a867525601c7186db9461846fcf7a4cb13
Mrugesh Mohapatra <noreply@mrugesh.dev>
2023-04-14 19:24:54 +0530

    fix(docker): improved configs and organisation
    This commmit moves the config files into a dedicated location for
    cleaner hoursekeeping, and management. It also makes the Caddyfile
    available to the container in readonly mode.

--- scripts/copy-dockerignore.js
+++ scripts/copy-dockerignore.js
@@ -3,5 +3,5 @@ const { join } = require("path");
 const portMap = require("../port-map.json");
 
 Object.keys(portMap).forEach((name) => {
-  copyFileSync(join(__dirname, "../shared.dockerignore"), join(__dirname, `../apps/${name}/.dockerignore`));
+  copyFileSync(join(__dirname, "../docker/shared.dockerignore"), join(__dirname, `../apps/${name}/.dockerignore`));
 });
