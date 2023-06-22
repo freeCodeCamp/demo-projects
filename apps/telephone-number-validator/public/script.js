16fbfc151f55be0db1bc38c93b1ab0b1d43fe0e1
scissorsneedfoodtoo <scissorsneedfoodtoo@gmail.com>
2023-06-22 17:25:56 +0900

    feat: add telephone number validator project

---
+++ apps/telephone-number-validator/public/script.js
@@ -0,0 +1,46 @@
+const userInput = document.getElementById('user-input');
+const checkBtn = document.getElementById('check-btn');
+const clearBtn = document.getElementById('clear-btn');
+const resultDiv = document.getElementById('results-div');
+
+const checkValidNumber = input => {
+  if (input === '') {
+    alert('Please provide a phone number');
+    return;
+  }
+  const countryCode = '^(1\\s?)?';
+  const areaCode = '(\\([0-9]{3}\\)|[0-9]{3})';
+  const spacesDashes = '[\\s\\-]?';
+  const phoneNumber = '[0-9]{3}[\\s\\-]?[0-9]{4}$';
+  const phoneRegex = new RegExp(
+    `${countryCode}${areaCode}${spacesDashes}${phoneNumber}`
+  );
+
+  const pTag = document.createElement('p');
+  pTag.className = 'results-text';
+  phoneRegex.test(input)
+    ? (pTag.style.color = '#00471b')
+    : (pTag.style.color = '#4d3800');
+  pTag.appendChild(
+    document.createTextNode(
+      `${phoneRegex.test(input) ? 'Valid' : 'Invalid'} U.S. number: ${input}`
+    )
+  );
+  resultDiv.appendChild(pTag);
+};
+
+checkBtn.addEventListener('click', () => {
+  checkValidNumber(userInput.value);
+  userInput.value = '';
+});
+
+userInput.addEventListener('keydown', e => {
+  if (e.key === 'Enter') {
+    checkValidNumber(userInput.value);
+    userInput.value = '';
+  }
+});
+
+clearBtn.addEventListener('click', () => {
+  resultDiv.textContent = '';
+});
