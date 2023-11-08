c33bca15c53440f54e4b1dad5ce86b4fa6b97022
a2937 <a.rcottrill521@gmail.com>
2023-11-08 00:23:37 -0500

    feat: live check on file metadata microservice (#499)

--- apps/file-metadata-microservice/server.js
+++ apps/file-metadata-microservice/server.js
@@ -5,7 +5,7 @@ var express = require('express');
 var cors = require('cors');
 
 var multer = require('multer');
-// here on HyperDev the fs is read only, 
+// here on HyperDev the fs is read only,
 // You have to upload the file to memory
 var storage = multer.memoryStorage();
 var upload = multer({ storage: storage });
@@ -16,29 +16,32 @@ app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
-    
-  // using 'multer' middleware...
-app.post('/api/fileanalyse',upload.single('upfile'), function(req, res){
-   res.json({
-    'name' : req.file.originalname,
-    'type' : req.file.mimetype,
-    'size' : req.file.size
-   });
+  res.sendFile(process.cwd() + '/views/index.html');
 });
-    
 
- // 404-NOT FOUND Middleware
- app.use(function(req, res){
-   res.status(404);
-   res.type('txt').send('Not found');
- });
+app.get('/status/ping', (req, res) => {
+  res.send({ msg: 'pong' }).status(200);
+});
+
+// using 'multer' middleware...
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+});
+
+// 404-NOT FOUND Middleware
+app.use(function (req, res) {
+  res.status(404);
+  res.type('txt').send('Not found');
+});
 
 const portNum = process.env.PORT || 3000;
 
 app.listen(portNum, function () {
-  console.log("Listening on port " + portNum);
+  console.log('Listening on port ' + portNum);
 });
 
 module.exports = app;
