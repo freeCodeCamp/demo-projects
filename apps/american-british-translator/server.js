16aeb6c3bb7643eb03126699a6dc76fe4573951d
a2937 <a.rcottrill521@gmail.com>
2023-11-09 00:50:23 -0500

    feat: add a live service check to the american-british translator (#498)

--- apps/american-british-translator/server.js
+++ apps/american-british-translator/server.js
@@ -1,37 +1,38 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors        = require('cors');
+const cors = require('cors');
 
-const fccTestingRoutes  = require('./routes/fcctesting.js');
-const runner            = require('./test-runner');
-const userRoutes        = require('./routes/api.js');
+const fccTestingRoutes = require('./routes/fcctesting.js');
+const runner = require('./test-runner');
+const userRoutes = require('./routes/api.js');
 
 const app = express();
 
 app.use('/public', express.static(process.cwd() + '/public'));
-app.use(cors({origin: '*'})); //For FCC testing purposes only
+app.use(cors({ origin: '*' })); //For FCC testing purposes only
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Index page (static HTML)
-app.route('/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-  }); 
+app.route('/').get(function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.get('/status/ping', (req, res) => {
+  res.send({ msg: 'pong' }).status(200);
+});
 
 //For FCC testing purposes
 fccTestingRoutes(app);
 
 // User routes
 userRoutes(app);
-    
+
 // 404 Not Found Middleware
-app.use(function(req, res) {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
+app.use(function (req, res) {
+  res.status(404).type('text').send('Not Found');
 });
 
 const portNum = process.env.PORT || 3000;
@@ -39,7 +40,7 @@ const portNum = process.env.PORT || 3000;
 // Start our server and tests!
 app.listen(portNum, () => {
   console.log(`Listening on port ${portNum}`);
-  if (process.env.NODE_ENV==='test') {
+  if (process.env.NODE_ENV === 'test') {
     console.log('Running Tests...');
     setTimeout(function () {
       try {
