53ba4aea76ed26599281c78b522b6753ab7fa699
a2937 <a.rcottrill521@gmail.com>
2023-11-10 00:37:15 -0500

    feat: live service check on stock price checker proxy (#507)

--- apps/stock-price-checker-proxy/server.js
+++ apps/stock-price-checker-proxy/server.js
@@ -1,27 +1,31 @@
-require("dotenv").config();
-const express = require("express");
+require('dotenv').config();
+const express = require('express');
 const app = express();
 
-const api_v1 = require("./api/v1");
+const api_v1 = require('./api/v1');
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.use("/v1", api_v1);
+app.use('/v1', api_v1);
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/index.html");
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/status/ping', (req, res) => {
+  res.send({ msg: 'pong' }).status(200);
 });
 
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
   if (err) {
     console.log(err.message, err.stack);
-    res.status(500).json({ status: "internal server error" });
+    res.status(500).json({ status: 'internal server error' });
   }
 });
 
 const portNum = process.env.PORT || 3000;
 
 app.listen(portNum, () => {
-  console.log("stock proxy is listening on port " + portNum);
+  console.log('stock proxy is listening on port ' + portNum);
 });
