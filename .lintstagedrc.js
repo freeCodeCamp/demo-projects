b9f26139ecbad2f837b955e9132389bb2c2896ce
Aaron <a.rcottrill521@gmail.com>
2023-11-11 12:35:17 -0500

    Feature: Live service check weather proxy

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,12 +1,8 @@
 const { ESLint } = require('eslint');
 
 const cli = new ESLint();
-
-// This lets us abort if we've already run a stage for all files
 const completedStages = new Set();
 
-// if a lot of files are changed, it's faster to run prettier/eslint on the
-// whole project than to run them on each file separately
 module.exports = {
   '*.(js|jsx|ts|tsx)': async files => {
     if (completedStages.has('js')) return [];
@@ -15,13 +11,16 @@ module.exports = {
       files.map(file => cli.isPathIgnored(file))
     );
     const lintableFiles = files.filter((_, i) => !ignoredIds[i]);
+
     if (files.length > 10) {
       completedStages.add('js');
       return ['eslint --max-warnings=0 --cache --fix .', 'prettier --write .'];
     } else {
       return [
-        'eslint --max-warnings=0 --cache --fix ' + lintableFiles.join(' '),
-        ...files.map(filename => `prettier --write '${filename}'`)
+        `eslint --max-warnings=0 --cache --fix ${lintableFiles
+          .map(file => `"${file}"`)
+          .join(' ')}`,
+        ...files.map(filename => `prettier --write "${filename}"`)
       ];
     }
   },
@@ -33,7 +32,7 @@ module.exports = {
       return 'prettier --write .';
     } else {
       return files.map(
-        filename => `prettier --write --ignore-unknown '${filename}'`
+        filename => `prettier --write --ignore-unknown "${filename}"`
       );
     }
   }
