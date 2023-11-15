6713127f46c7f876ad17ebf9d79dcd9230f563a8
a2937 <a.rcottrill521@gmail.com>
2023-11-15 10:35:59 -0500

    feat: Live check for exercise tracker (#518)

--- apps/exercise-tracker/server.js
+++ apps/exercise-tracker/server.js
@@ -7,15 +7,21 @@ const mongoose = require('mongoose');
 
 mongoose.set('useUnifiedTopology', true);
 mongoose.set('useCreateIndex', true);
-mongoose.connect(process.env.DB_URI || 'mongodb://localhost/exercise-track', { useNewUrlParser: true });
+mongoose.connect(process.env.DB_URI || 'mongodb://localhost/exercise-track', {
+  useNewUrlParser: true
+});
 
-app.use(express.urlencoded({'extended': false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/status/ping', (req, res) => {
+  res.status(200).send({ msg: 'pong' });
 });
 
 const apiRouter = require('./routes/api');
@@ -23,12 +29,12 @@ app.use('/api', apiRouter);
 
 // Not found middleware
 app.use((req, res, next) => {
-  return next({status: 404, message: 'not found'});
-})
+  return next({ status: 404, message: 'not found' });
+});
 
 // Error Handling middleware
 app.use((err, req, res) => {
-  let errCode, errMessage
+  let errCode, errMessage;
 
   if (err.errors) {
     // Mongoose validation error
@@ -41,8 +47,7 @@ app.use((err, req, res) => {
     errCode = err.status || 500;
     errMessage = err.message || 'Internal Server Error';
   }
-  res.status(errCode).type('txt')
-    .send(errMessage);
+  res.status(errCode).type('txt').send(errMessage);
 });
 
 const portNum = process.env.PORT || 3000;
