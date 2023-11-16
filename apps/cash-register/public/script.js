d1d1a0500ed1ba0f32704ebdaf56082e16c5e054
scissorsneedfoodtoo <scissorsneedfoodtoo@gmail.com>
2023-11-16 14:16:52 +0900

    fix: tweak alert messages for cash register

--- apps/cash-register/public/script.js
+++ apps/cash-register/public/script.js
@@ -1,15 +1,15 @@
 // Do not change code below this line
 let price = 19.5;
 let cid = [
-  ['PENNY', 1.01],
-  ['NICKEL', 2.05],
-  ['DIME', 3.1],
-  ['QUARTER', 4.25],
-  ['ONE', 90],
-  ['FIVE', 55],
-  ['TEN', 20],
-  ['TWENTY', 60],
-  ['ONE HUNDRED', 100]
+  ['PENNY', 0.5],
+  ['NICKEL', 0],
+  ['DIME', 0],
+  ['QUARTER', 0],
+  ['ONE', 0],
+  ['FIVE', 0],
+  ['TEN', 0],
+  ['TWENTY', 0],
+  ['ONE HUNDRED', 0]
 ];
 // Do not change code above this line
 
@@ -28,14 +28,14 @@ const formatResults = (status, change) => {
 
 const checkCashRegister = () => {
   if (Number(cash.value) < price) {
-    alert('Customer does not have enough money to purchase item.');
+    alert('Customer does not have enough money to purchase the item');
     cash.value = '';
     return;
   }
 
   if (Number(cash.value) === price) {
     displayChangeDue.innerHTML =
-      'No change due. Customer paid with exact cash.';
+      'No change due - customer paid with exact cash';
     cash.value = '';
     return;
   }
