462f100c3235bb3ed260a2fb13fad6bea73ca764
a2937 <a.rcottrill521@gmail.com>
2023-11-19 12:29:28 -0500

    feat: Live check heat map (#525)

--- apps/heat-map/server.js
+++ apps/heat-map/server.js
@@ -8,6 +8,10 @@ app.get('', (req, res) => {
   res.sendFile(`${process.cwd()}/index.html`);
 });
 
+app.get('/status/ping', (req, res) => {
+  res.status(200).send({ msg: 'pong' });
+});
+
 const portNum = process.env.PORT || 3000;
 
 app.listen(portNum, () => {
