983205d5f9a9f2f96e28fa43e194f896431de212
a2937 <a.rcottrill521@gmail.com>
2023-11-20 18:22:17 -0500

    feat: Live check on p2p video chat app (#529)

--- apps/p2p-video-chat-application/server.js
+++ apps/p2p-video-chat-application/server.js
@@ -1,26 +1,30 @@
-require("dotenv").config();
-const express = require("express");
+require('dotenv').config();
+const express = require('express');
 const app = express();
-const server = require("http").createServer(app);
-const { Server } = require("socket.io");
+const server = require('http').createServer(app);
+const { Server } = require('socket.io');
 
 const io = new Server(server);
 
-app.use(express.static("public"));
+app.use(express.static('public'));
+
+app.get('/status/ping', (req, res) => {
+  res.status(200).send({ msg: 'pong' });
+});
 
 const portNum = process.env.PORT || 3000;
 
 server.listen(portNum, function () {
-  console.log("Listening on port " + portNum);
+  console.log('Listening on port ' + portNum);
 });
 
-io.on("connection", function (socket) {
-  socket.on("create or join", function (room) {
+io.on('connection', function (socket) {
+  socket.on('create or join', function (room) {
     io.in(room)
       .allSockets()
       .then(function (clients) {
         if (clients.size >= 2) {
-          socket.emit("errorMessage", `Could not join room. It's full!`);
+          socket.emit('errorMessage', `Could not join room. It's full!`);
           return;
         }
 
@@ -28,31 +32,33 @@ io.on("connection", function (socket) {
 
         const joinMessage = {
           initiator: true,
-          data: `You have successfully joined ${room}!`,
+          data: `You have successfully joined ${room}!`
         };
 
         if (clients.size == 0) {
-          socket.emit("joined room", joinMessage);
+          socket.emit('joined room', joinMessage);
         } else {
           joinMessage.initiator = false;
-          socket.emit("joined room", joinMessage);
-          socket.to(room).emit("newcomer", "Hi there people!");
+          socket.emit('joined room', joinMessage);
+          socket.to(room).emit('newcomer', 'Hi there people!');
         }
 
         console.log(`${room} room has ${clients.size} people`);
       });
   });
 
-  socket.on("roomMessage", function (roomMessage) {
-    io.in(roomMessage.room).allSockets().then(function (clients) {
-      if (clients.has(socket.id)) {
-        console.log(
-          `Client ${socket.id} sent room message : ${roomMessage.room} - ${roomMessage.data}`
-        );
-        socket.to(roomMessage.room).emit("roomMessage", roomMessage);
-      } else {
-        socket.emit("errorMessage", `You don't belong to this room kid!`);
-      }
-    });
+  socket.on('roomMessage', function (roomMessage) {
+    io.in(roomMessage.room)
+      .allSockets()
+      .then(function (clients) {
+        if (clients.has(socket.id)) {
+          console.log(
+            `Client ${socket.id} sent room message : ${roomMessage.room} - ${roomMessage.data}`
+          );
+          socket.to(roomMessage.room).emit('roomMessage', roomMessage);
+        } else {
+          socket.emit('errorMessage', `You don't belong to this room kid!`);
+        }
+      });
   });
 });
