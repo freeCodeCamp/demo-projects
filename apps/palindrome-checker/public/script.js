5ec7aaef78581771f720a7cd61cb00bbf195f68d
scissorsneedfoodtoo <scissorsneedfoodtoo@gmail.com>
2023-11-22 15:54:55 +0900

    fix: rename id from btn to check-btn in palindrome checker

--- apps/palindrome-checker/public/script.js
+++ apps/palindrome-checker/public/script.js
@@ -1,5 +1,5 @@
 const userInput = document.getElementById('text-input');
-const checkPalindromeBtn = document.getElementById('btn');
+const checkPalindromeBtn = document.getElementById('check-btn');
 const resultDiv = document.getElementById('result');
 
 const checkForPalindrome = input => {
