2d042aeaec3222daac91b5964bc21ef0ae2b0240
a2937 <a.rcottrill521@gmail.com>
2023-12-01 20:41:47 -0500

    feat: Live check on Sudoku Solver (#544)

--- apps/sudoku-solver/routes/api.js
+++ apps/sudoku-solver/routes/api.js
@@ -1,80 +1,83 @@
 /*
-*
-*
-*       Complete the API routing below
-*
-*
-*/
+ *
+ *
+ *       Complete the API routing below
+ *
+ *
+ */
 
 'use strict';
 
 const SudokuSolver = require('../controllers/sudoku-solver.js');
 
 module.exports = function (app) {
-  
   let solver = new SudokuSolver();
 
-  app.route('/api/check')
-    .post((req, res) => {
-      if(!req.body.puzzle || !req.body.coordinate || !req.body.value) {
-        return res.json({ error: 'Required field(s) missing' });
-      }
+  app.route('/status/ping').get(function (req, res) {
+    res.send({ msg: 'pong' }).status(200);
+  });
 
-      let [err, errorString] = solver.validate(req.body.puzzle);
-      if(err) {
-        return res.json( { error: errorString });
-      }
+  app.route('/api/check').post((req, res) => {
+    if (!req.body.puzzle || !req.body.coordinate || !req.body.value) {
+      return res.json({ error: 'Required field(s) missing' });
+    }
 
-      let value = parseInt(req.body.value);
-      if(!Number.isInteger(value) || value < 1 || value > 9) {
-        return res.json({ error: 'Invalid value' });
-      }
+    let [err, errorString] = solver.validate(req.body.puzzle);
+    if (err) {
+      return res.json({ error: errorString });
+    }
 
-      let [coord_err, row, col] = solver.getCoordinate(req.body.coordinate);
-      if(coord_err) {
-        return res.json({error: 'Invalid coordinate'})
-      }
+    let value = parseInt(req.body.value);
+    if (!Number.isInteger(value) || value < 1 || value > 9) {
+      return res.json({ error: 'Invalid value' });
+    }
 
-      const puzzle = solver.clearTargetCellInPuzzle(req.body.puzzle, row, col);
+    let [coord_err, row, col] = solver.getCoordinate(req.body.coordinate);
+    if (coord_err) {
+      return res.json({ error: 'Invalid coordinate' });
+    }
 
-      let rowCheck = solver.checkRowPlacement(puzzle, row, value);
-      let colCheck = solver.checkColPlacement(puzzle, col, value);
-      let regionCheck = solver.checkRegionPlacement(puzzle,
-        row - row % 3,
-        col - col % 3,
-        value);
+    const puzzle = solver.clearTargetCellInPuzzle(req.body.puzzle, row, col);
 
-      if(rowCheck && colCheck && regionCheck) {
-        return res.json( { valid: true });
-      } else {
-        let result = [];
-        
-        if(!rowCheck) result.push('row');
-        if(!colCheck) result.push('column');
-        if(!regionCheck) result.push('region');
+    let rowCheck = solver.checkRowPlacement(puzzle, row, value);
+    let colCheck = solver.checkColPlacement(puzzle, col, value);
+    let regionCheck = solver.checkRegionPlacement(
+      puzzle,
+      row - (row % 3),
+      col - (col % 3),
+      value
+    );
 
-        return res.json( {
-          valid: false,
-          conflict: result
-        });
-      }
-    });
-    
-  app.route('/api/solve')
-    .post((req, res) => {
-      if(!req.body.puzzle) {
-        return res.json({ error: 'Required field missing' });
-      }
+    if (rowCheck && colCheck && regionCheck) {
+      return res.json({ valid: true });
+    } else {
+      let result = [];
 
-      let [err, errorString] = solver.validate(req.body.puzzle);
-      if(err) {
-        return res.json( { error: errorString });
-      }
+      if (!rowCheck) result.push('row');
+      if (!colCheck) result.push('column');
+      if (!regionCheck) result.push('region');
 
-      if(solver.solve(req.body.puzzle)) {
-        return res.json( { solution: solver.exportString() });
-      } else {
-        return res.json( { error: 'Puzzle cannot be solved' });
-      }
-    });
+      return res.json({
+        valid: false,
+        conflict: result
+      });
+    }
+  });
+
+  app.route('/api/solve').post((req, res) => {
+    if (!req.body.puzzle) {
+      return res.json({ error: 'Required field missing' });
+    }
+
+    let [err, errorString] = solver.validate(req.body.puzzle);
+    if (err) {
+      return res.json({ error: errorString });
+    }
+
+    if (solver.solve(req.body.puzzle)) {
+      return res.json({ solution: solver.exportString() });
+    } else {
+      return res.json({ error: 'Puzzle cannot be solved' });
+    }
+  });
 };
