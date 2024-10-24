62c75cb987d66feb8e946624b6208f21a833fe14
Shaun Hamilton <shauhami020@gmail.com>
2024-10-24 08:27:03 +0000

    chore: set url for gitpod testing

--- test/twitch-proxy.test.js
+++ test/twitch-proxy.test.js
@@ -1,25 +1,27 @@
 // TODO: Remove once types for Jest are recognized
 /* eslint-disable no-undef */
-const axios = require('axios');
 const portMap = require('../port-map.json');
 const twitchProxyPort = portMap['twitch-proxy'];
+const { baseUrl } = require('./jest-utils.js');
+
+const BASE_URL = baseUrl(twitchProxyPort);
 
 describe('kraken api', () => {
   it('should return freecodecamp user data', async () => {
-    const response = await axios.get(
-      `http://localhost:${twitchProxyPort}/twitch-api/users/freecodecamp`
+    const response = await fetch(
+      new URL('/twitch-api/users/freecodecamp', BASE_URL)
     );
-    const user = response.data;
+    const user = await response.json();
     expect(response.status).toBe(200);
     expect(user.name).toBe('freecodecamp');
     expect(user.display_name).toBe('FreeCodeCamp');
   });
 
   it('should return freecodecamp channel data', async () => {
-    const response = await axios.get(
-      `http://localhost:${twitchProxyPort}/twitch-api/channels/freecodecamp`
+    const response = await fetch(
+      new URL('/twitch-api/channels/freecodecamp', BASE_URL)
     );
-    const channel = response.data;
+    const channel = await response.json();
     expect(response.status).toBe(200);
     expect(channel.name).toBe('freecodecamp');
     expect(channel.display_name).toBe('FreeCodeCamp');
@@ -27,10 +29,10 @@ describe('kraken api', () => {
   });
 
   it("should return esl_sc2's stream data", async () => {
-    const response = await axios.get(
-      `http://localhost:${twitchProxyPort}/twitch-api/streams/ESL_SC2`
+    const response = await fetch(
+      new URL('/twitch-api/streams/ESL_SC2', BASE_URL)
     );
-    const stream = response.data.stream;
+    const stream = (await response.json()).stream;
     expect(response.status).toBe(200);
     expect(stream._id).toBe(23366709968);
   });
